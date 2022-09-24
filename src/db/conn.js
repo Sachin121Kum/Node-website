6df60d06a37f1363a16d4625a217d6ffae0f3a06
Sachin121kum <sk31817@gmail.com>
2022-09-25 01:32:58 +0530

    Connectivity Complete

---
+++ src/db/conn.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/nodeweb",{
+    useUnifiedTopology:true
+}).then(()=> {
+    console.log("Connection Successful*");
+}).catch((error) =>{
+    console.log(error);
+});
